0e1e6cfaa3b3fe775eb58ce917394c7d068cc18c
Iain Galloway <iain.galloway@gmail.com>
2023-02-21 15:06:20 +0000

    Add git config

--- setup-env.sh
+++ setup-env.sh
@@ -5,3 +5,6 @@ ln -sf "$REPO_LOCATION/config/bash-aliases" "$HOME/.config/.bash-aliases"
 
 mkdir -p "$HOME/.config"
 ln -sf "$REPO_LOCATION/config/starship.toml" "$HOME/.config/starship.toml"
+
+mkdir -p "$HOME/.config/git"
+ln -sf "$REPO_LOCATION/config/git/config" "$HOME/.config/git/config"
