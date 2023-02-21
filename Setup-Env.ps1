5e5b0b571281a3e826ae142ba6affc0947b49dc9
Iain Galloway <iain.galloway@gmail.com>
2023-02-21 12:57:01 +0000

    Suppressed output from New-Item in Setup-Env.ps1

--- Setup-Env.ps1
+++ Setup-Env.ps1
@@ -1,4 +1,4 @@
 $ConfigDir = Join-Path $env:USERPROFILE ".config"
 
-New-Item -ItemType Directory -Path $ConfigDir -Force
-New-Item -ItemType SymbolicLink -Path (Join-Path $ConfigDir "starship.toml") -Target (Join-Path $PSScriptRoot "starship.toml") -Force
+New-Item -ItemType Directory -Path $ConfigDir -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path (Join-Path $ConfigDir "starship.toml") -Target (Join-Path $PSScriptRoot "starship.toml") -Force | Out-Null
