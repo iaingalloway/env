74af7f443493bd8b9a6db4f51010438dce41ca67
Iain Galloway <iain.galloway@gmail.com>
2023-02-21 14:23:23 +0000

    Update path for starship.toml in Setup-Env.ps1

--- Setup-Env.ps1
+++ Setup-Env.ps1
@@ -1,6 +1,6 @@
 $ConfigLocation = Join-Path $env:USERPROFILE ".config"
 New-Item -ItemType Directory -Path $ConfigLocation -Force | Out-Null
-New-Item -ItemType SymbolicLink -Path (Join-Path $ConfigLocation "starship.toml") -Target (Join-Path $PSScriptRoot "starship.toml") -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path (Join-Path $ConfigLocation "config\starship.toml") -Target (Join-Path $PSScriptRoot "starship.toml") -Force | Out-Null
 
 $PowerShellProfileLocation = Join-Path $env:USERPROFILE "Documents\PowerShell"
 New-Item -ItemType Directory -Path $PowerShellProfileLocation -Force | Out-Null
