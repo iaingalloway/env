1f7a97bb464ebe031b083df245850e1be8a4a696
Iain Galloway <iain.galloway@gmail.com>
2023-02-21 13:24:29 +0000

    Added powershell profile

--- Setup-Env.ps1
+++ Setup-Env.ps1
@@ -1,3 +1,7 @@
 $ConfigLocation = Join-Path $env:USERPROFILE ".config"
 New-Item -ItemType Directory -Path $ConfigLocation -Force | Out-Null
 New-Item -ItemType SymbolicLink -Path (Join-Path $ConfigLocation "starship.toml") -Target (Join-Path $PSScriptRoot "starship.toml") -Force | Out-Null
+
+$PowerShellProfileLocation = Join-Path $env:USERPROFILE "Documents\PowerShell"
+New-Item -ItemType Directory -Path $PowerShellProfileLocation -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path (Join-Path $PowerShellProfileLocation "Microsoft.PowerShell_profile.ps1") -Target (Join-Path $PSScriptRoot "Microsoft.PowerShell_profile.ps1") -Force | Out-Null
