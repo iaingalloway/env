f6abcf88c4fe77bccdf3dc5c0d4214d086fb4852
Iain Galloway <iain.galloway@gmail.com>
2023-02-21 14:14:00 +0000

    Added bash-aliases

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,7 @@
 REPO_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 ln -sf "$REPO_LOCATION/bashrc" "$HOME/.bashrc"
+ln -sf "$REPO_LOCATION/bash-aliases" "$HOME/.bash-aliases"
 
 mkdir -p "$HOME/.config"
 ln -sf "$REPO_LOCATION/starship.toml" "$HOME/.config/starship.toml"
