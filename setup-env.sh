c7826dd95245b2bc8e4680e7f3e618d5040f5b1e
Iain Galloway <iain.galloway@gmail.com>
2023-02-21 14:22:17 +0000

    Move files to config directory

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,7 @@
 REPO_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 ln -sf "$REPO_LOCATION/bashrc" "$HOME/.bashrc"
-ln -sf "$REPO_LOCATION/bash-aliases" "$HOME/.bash-aliases"
+ln -sf "$REPO_LOCATION/config/bash-aliases" "$HOME/.config/.bash-aliases"
 
 mkdir -p "$HOME/.config"
-ln -sf "$REPO_LOCATION/starship.toml" "$HOME/.config/starship.toml"
+ln -sf "$REPO_LOCATION/config/starship.toml" "$HOME/.config/starship.toml"
