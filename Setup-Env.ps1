71ebc0d8a9536866fd54bf9fe3ce407d19256445
Iain Galloway <iain.galloway@gmail.com>
2023-03-05 10:44:28 +0000

    Moved powershell settings

--- Setup-Env.ps1
+++ Setup-Env.ps1
@@ -1,6 +1,6 @@
 $PowerShellProfileLocation = Join-Path $env:USERPROFILE "Documents\PowerShell"
 New-Item -ItemType Directory -Path $PowerShellProfileLocation -Force | Out-Null
-New-Item -ItemType SymbolicLink -Path (Join-Path $PowerShellProfileLocation "Microsoft.PowerShell_profile.ps1") -Target (Join-Path $PSScriptRoot "documents\powershell\powershell-profile.ps1") -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path (Join-Path $PowerShellProfileLocation "Microsoft.PowerShell_profile.ps1") -Target (Join-Path $PSScriptRoot "config\powershell\powershell-profile.ps1") -Force | Out-Null
 
 $ConfigLocation = Join-Path $env:USERPROFILE ".config"
 New-Item -ItemType Directory -Path $ConfigLocation -Force | Out-Null
