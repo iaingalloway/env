a69716556a88bc63677b2f6d8cad7ade3fd11606
Iain Galloway <iain.galloway@gmail.com>
2023-03-19 11:21:54 +0000

    Add aliases for powershell

--- config/powershell/powershell-profile.ps1
+++ config/powershell/powershell-profile.ps1
@@ -1,1 +1,6 @@
 Invoke-Expression (&starship init powershell)
+
+New-Alias k kubectl
+New-Alias kclear kubectl config unset current-context
+New-Alias kuse kubectl config use-context
+New-Alias g git
