8af4a54b2303d999f4a7b5daba45cf27fa2f7932
Iain Galloway <iain.galloway@gmail.com>
2023-03-29 10:29:32 +0100

    Fix kubectl aliases, completion, added gs and ga aliases for git

--- config/powershell/powershell-profile.ps1
+++ config/powershell/powershell-profile.ps1
@@ -1,6 +1,31 @@
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Invoke-Expression (&starship init powershell)
 
-New-Alias k kubectl
-New-Alias kclear 'kubectl config unset current-context'
-New-Alias kuse 'kubectl config use-context'
+# git
 New-Alias g git
+
+function gs { git status $args }
+function ga { git add $args }
+
+# kubectl
+$KubectlCompletionScript = (kubectl completion powershell)
+$KubectlCompletionScript | Out-String | Invoke-Expression 
+
+New-Alias k kubectl
+$KubectlAliasCompletionScript = $KubectlCompletionScript -replace "kubectl", "k"
+$KubectlAliasCompletionScript | Out-String | Invoke-Expression
+
+function kclear { kubectl config unset current-context }
+function kuse {
+    param(
+        [string]$Context
+    )
+
+    kubectl config use-context $Context
+}
+
+Register-ArgumentCompleter -CommandName kuse -ParameterName Context -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    $Contexts = (kubectl config get-contexts --output='name') | Where-Object { $_ -like "$wordToComplete*" }
+    return $Contexts
+}
