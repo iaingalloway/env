cbd72aa2320007b99f852c18df2fb695f9c39d69
Iain Galloway <iain.galloway@gmail.com>
2023-03-29 21:59:06 +0100

    Add shell completion for winget

--- config/powershell/powershell-profile.ps1
+++ config/powershell/powershell-profile.ps1
@@ -37,3 +37,14 @@ Register-ArgumentCompleter -CommandName kuse -ParameterName Context -ScriptBlock
     $Contexts = (kubectl config get-contexts --output='name') | Where-Object { $_ -like "$wordToComplete*" }
     return $Contexts
 }
+
+#winget
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
