2d407c03cfe11f74f65582a546fda7272d4555db
Iain Galloway <iain.galloway@gmail.com>
2023-07-28 07:16:50 +0100

    Move gitconfig file on bash to ~/.gitconfig because this is the default path for mounting to devcontainers

--- setup/bash/link-config-files.sh
+++ setup/bash/link-config-files.sh
@@ -11,4 +11,4 @@ ln -sf "$REPO_LOCATION/config/bash-aliases" "$HOME/.config/.bash-aliases"
 ln -sf "$REPO_LOCATION/config/starship.toml" "$HOME/.config/starship.toml"
 
 mkdir -p "$HOME/.config/git"
-ln -sf "$REPO_LOCATION/config/git/config" "$HOME/.config/git/config"
+ln -sf "$REPO_LOCATION/config/git/config" "$HOME/.gitconfig"
