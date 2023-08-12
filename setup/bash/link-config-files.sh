39817374da5368830a98037d5c09f72efb0d61ae
Iain Galloway <iain.galloway@gmail.com>
2023-08-12 15:18:42 +0100

    Update location for git config

--- setup/bash/link-config-files.sh
+++ setup/bash/link-config-files.sh
@@ -6,9 +6,8 @@ REPO_LOCATION=$1
 
 ln -sf "$REPO_LOCATION/bashrc" "$HOME/.bashrc"
 
+ln -sf "$REPO_LOCATION/config/git/config" "$HOME/.gitconfig"
+
 mkdir -p "$HOME/.config"
 ln -sf "$REPO_LOCATION/config/bash-aliases" "$HOME/.config/.bash-aliases"
 ln -sf "$REPO_LOCATION/config/starship.toml" "$HOME/.config/starship.toml"
-
-mkdir -p "$HOME/.config/git"
-ln -sf "$REPO_LOCATION/config/git/config" "$HOME/.gitconfig"
--- setup/powershell/Link-ConfigFiles.ps1
+++ setup/powershell/Link-ConfigFiles.ps1
@@ -11,9 +11,7 @@ $ConfigLocation = Join-Path $env:USERPROFILE ".config"
 New-Item -ItemType Directory -Path $ConfigLocation -Force | Out-Null
 New-Item -ItemType SymbolicLink -Path (Join-Path $ConfigLocation "starship.toml") -Target (Join-Path $RepoLocation "\config\starship.toml") -Force | Out-Null
 
-$GitConfigLocation = Join-Path $ConfigLocation "git"
-New-Item -ItemType Directory -Path $GitConfigLocation -Force | Out-Null
-New-Item -ItemType SymbolicLink -Path (Join-Path $GitConfigLocation "config") -Target (Join-Path $RepoLocation "\config\git\config") -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path (Join-Path $env:USERPROFILE ".gitconfig") -Target (Join-Path $RepoLocation "config\git\config") -Force | Out-Null
 
 $WindowsTerminalSettingsLocation = Join-Path $env:LOCALAPPDATA "Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 New-Item -ItemType Directory -Path $WindowsTerminalSettingsLocation -Force | Out-Null
