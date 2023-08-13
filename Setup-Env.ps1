386c6d9d2166592b8b76c707fe8a7ac7ddcf182b
Iain Galloway <iain.galloway@gmail.com>
2023-08-13 11:19:24 +0100

    Check for elevated permissions in Setup-Env.ps1

--- Setup-Env.ps1
+++ Setup-Env.ps1
@@ -1,3 +1,8 @@
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+  Write-Error "This script requires elevated permissions. Please run as Administrator."
+  exit 1
+}
+
 $RepoLocation = $PSScriptRoot
 
 .\setup\powershell\Link-ConfigFiles.ps1 $RepoLocation
