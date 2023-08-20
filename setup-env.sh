693ee9c96a289ff7e1360d1db48df033deb598d2
Iain Galloway <iain.galloway@gmail.com>
2023-08-20 17:21:36 +0100

    Correct path for link-config-files.sh in setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 REPO_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-./setup/bash/link-config-files.sh "$REPO_LOCATION"
+"$REPO_LOCATION/setup/bash/link-config-files.sh" "$REPO_LOCATION"
